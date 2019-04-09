cb5600eb42b1d10b73d57891dc165756218db323
Stanislav Menzulski <menzulski@gmail.com>
2019-04-10 00:43:04 +0300

    Prepare certs export with api

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('trainigDirections/', views.ListTrainigDirectionsView.as_view(), name="trainigDirections-all"),
     path('allCerts/', views.certificates, name="api-allCerts"),
     path('issuedCerts/', views.issuedCerts, name="api-issuedCerts"),
-    path('changeCertNumber/', views.changeCertNumber, name="api-changeCertNumber")
+    path('changeCertNumber/', views.changeCertNumber, name="api-changeCertNumber"),
+    path('exportXLS/', views.exportXLS, name='api-exportXLS'),
 ]
