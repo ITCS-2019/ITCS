6e979eeef9b29363200d781baccaa65eb72550bd
Andrew Borodyn <andbor1rey@gmail.com>
2019-04-11 15:27:35 +0300

    new sailor certificated grid

---
+++ static/js/templates/dev_extreme_grids/sailorsCertificatedGrid.js
@@ -0,0 +1,98 @@
+$(function() {
+    if ($('#sailors-certificated-grid').length > 0) {
+        DevExpress.localization.locale('ru');
+
+        let trainingDirectionsGrid = $('#sailors-certificated-grid').dxDataGrid({
+            dataSource: certificates,
+            allowColumnReordering: false,
+            allowColumnResizing: true,
+            columnAutoWidth: false,
+            showBorders: true,
+            showRowLines: true,
+            paging: {
+                enabled: true,
+                pageSize: 10
+            },
+            pager: {
+                showPageSizeSelector: true,
+                allowedPageSizes: [10, 20, 50, 100],
+                showInfo: false
+            },
+            searchPanel: {
+                visible: true,
+                width: 240,
+                placeholder: "Шукати..."
+            },
+            wordWrapEnabled: true,
+            columnAutoWidth: true,
+            onContentReady: function(e) {
+                function changePage(page) {
+                    e.component.pageIndex(page);
+                }
+
+                let $customPagination = $('.custom-pagination.custom-pagination--certificates'),
+                    $select = $('.custom-pagination__select', $customPagination),
+                    pageCount = e.component.pageCount(),
+                    currentPage = e.component.pageIndex(),
+                    $firstPageBtn = $('.custom-pagination__btn--first-page', $customPagination),
+                    $lastPageBtn = $('.custom-pagination__btn--last-page', $customPagination),
+                    $nextPageBtn = $('.custom-pagination__btn--next', $customPagination),
+                    $prevPageBtn = $('.custom-pagination__btn--prev', $customPagination),
+                    $gridToolbar = (e.element.find('.dx-toolbar-items-container').length > 0)
+                        ? e.element.find('.dx-toolbar-items-container')
+                        : e.element.find('.dx-datagrid-header-panel');
+
+                if (pageCount > 1) {
+                    $select.empty();
+                    for (let i = 0; i < pageCount; i++) {
+                        (i === currentPage)
+                            ? $select.append(`<option selected="selected" value="${i}">${i + 1}</option>>`)
+                            : $select.append(`<option value="${i}">${i + 1}</option>>`);
+                    }
+
+                    $gridToolbar.append($customPagination);
+
+                    if (currentPage === 0) {
+                        $firstPageBtn.attr('disabled', true);
+                        $prevPageBtn.attr('disabled', true);
+                    }
+                    else {
+                        $firstPageBtn.attr('disabled', false);
+                        $prevPageBtn.attr('disabled', false);
+                    }
+
+                    if ((currentPage + 1) === pageCount) {
+                        $lastPageBtn.attr('disabled', true);
+                        $nextPageBtn.attr('disabled', true);
+                    }
+                    else {
+                        $lastPageBtn.attr('disabled', false);
+                        $nextPageBtn.attr('disabled', false);
+                    }
+
+                    $select.on('change', function() {changePage($(this).val(), pageCount)});
+                    $firstPageBtn.on('click', function() {changePage(0, pageCount)});
+                    $lastPageBtn.on('click', function() {changePage(pageCount - 1)});
+                    $nextPageBtn.on('click', function() {changePage(currentPage + 1)});
+                    $prevPageBtn.on('click', function() {changePage(currentPage - 1)});
+
+                    $customPagination.fadeIn('fast');
+                }
+            },
+            columns: [
+                {
+                    dataField: 'training_direction',
+                    caption: 'Направління'
+                },
+                {
+                    dataField: 'last_name_ukr',
+                    caption: 'Прiзвище'
+                },
+                {
+                    dataField: 'first_name_ukr',
+                    caption: 'Iм\'я'
+                }
+            ]
+        }).dxDataGrid('instance');
+    }
+});
