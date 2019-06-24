ba0e2237335917eb8b643cc9f48c3288c1e444c4
Andrew Borodyn <andbor1rey@gmail.com>
2019-06-24 10:23:25 +0300

    change status in training direction grid fix

---
+++ front/src/components/forms/TrainingOrganisation.vue
@@ -0,0 +1,85 @@
+<template>
+    <v-form>
+        <v-container py-0>
+            <v-tabs fixed-tabs>
+                <v-tab v-for="lang in organisation.langs"
+                :key="lang">
+                    {{lang.lang}}
+                </v-tab>
+
+                <v-tab-item
+                v-for="lang in organisation.langs"
+                :key="lang">
+                    <v-layout wrap>
+                        <v-flex xs12>
+                            {{lang.lang}}
+                            <v-text-field label="Назва НТЗ"/>
+                        </v-flex>
+                        <v-flex xs12>
+                            <v-text-field label="Email адреса"
+                            prepend-inner-icon="mdi-email-outline"/>
+                        </v-flex>
+                    </v-layout>
+                </v-tab-item>
+            </v-tabs>
+            <v-layout wrap>
+                <v-flex xs12
+                text-xs-right>
+                    <v-btn class="mx-0"
+                    v-on:click="$router.go(-1)"
+                    color="success"
+                    flat>
+                        Вiдхилити
+                    </v-btn>
+                    <v-btn class="mx-0 font-weight-light ml-1"
+                    color="success">
+                        Зберегти
+                    </v-btn>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+export default {
+  name: 'TrainingOrganisationInfo',
+  props: {
+    organisationData: {
+      type: Object,
+      required: false,
+      default: () => {}
+    }
+  },
+
+  data(){
+    return {
+      organisation: {
+        logo: null,
+        phones: [],
+        email: null,
+        langs: [
+          {
+            lang: 'Українська',
+            head_name: null,
+            head_position: null
+          },
+          {
+            lang: 'Англiйська',
+            director_name: null,
+            head_position: null
+          }
+        ]
+      }
+    }
+  },
+
+  created() {
+
+    },
+
+  mounted() {
+
+  }
+}
+</script>
