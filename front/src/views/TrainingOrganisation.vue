958dc442683a52965c4db0ed2b5fea4e275c12b4
Andrew Borodyn <andbor1rey@gmail.com>
2019-07-12 15:41:24 +0300

    give certs

--- front/src/views/TrainingOrganisation.vue
+++ front/src/views/TrainingOrganisation.vue
@@ -353,12 +353,28 @@
                 });
 
                 if (isSameSpecialties) {
-                  let element = document.createElement('a');
-                  element.setAttribute('href', `/mariner/api/exportXLS?exportType=${_this.exportType}&certIDs=${certIDs.join(',')}`);
-                  element.style.display = 'none';
-                  document.body.appendChild(element);
-                  element.click();
-                  document.body.removeChild(element);
+
+                  // TODO: Refactor Remove ifs
+                  if (_this.exportType === 'Register') {
+                    let element = document.createElement('a');
+                    element.setAttribute('href', `/mariner/api/exportXLS?exportType=${_this.exportType}&certIDs=${certIDs.join(',')}`);
+                    element.style.display = 'none';
+                    document.body.appendChild(element);
+                    element.click();
+                    document.body.removeChild(element);
+                  }
+                  else {
+                    axios.get(`/mariner/api/exportXLS?exportType=${_this.exportType}&certIDs=${certIDs.join(',')}`)
+                      .then(res => {
+                        _this.snackbarConfig.icon = 'mdi-check-circle';
+                        _this.snackbarConfig.color = 'success';
+                        _this.snackbarConfig.message = `Сертифiкати успiшно виданi!`;
+                        _this.snackbar = true;
+                      })
+                      .catch((err) => {
+                        console.log(err);
+                      });
+                  }
                 }
                 else {
                   _this.snackbarConfig.icon = 'mdi-alert-circle';
@@ -547,129 +563,6 @@
             }
           ]
         },
-        tableOrganisationConfig: {
-          dataSource: [],
-          allowColumnReordering: false,
-          allowColumnResizing: true,
-          columnAutoWidth: false,
-          showBorders: true,
-          showRowLines: true,
-          paging: {
-            enabled: true,
-            pageSize: 10
-          },
-          pager: {
-            showPageSizeSelector: true,
-            allowedPageSizes: [10, 20, 50, 100],
-            showInfo: true,
-            visible: true
-          },
-          searchPanel: {
-            visible: true,
-            width: 240,
-            placeholder: "Шукати..."
-          },
-          rowAlternationEnabled: true,
-          headerFilter: {
-            visible: false
-          },
-          filterRow: {
-            visible: false,
-            applyFilter: "auto"
-          },
-          hoverStateEnabled: true,
-          wordWrapEnabled: true,
-          columnAutoWidth: true,
-          onContentReady: function(e) {
-            function changePage(page) {
-              e.component.pageIndex(page);
-            }
-
-            let $customPagination = $('.custom-pagination.custom-pagination--certificates'),
-                    $select = $('.custom-pagination__select', $customPagination),
-                    pageCount = e.component.pageCount(),
-                    currentPage = e.component.pageIndex(),
-                    $firstPageBtn = $('.custom-pagination__btn--first-page', $customPagination),
-                    $lastPageBtn = $('.custom-pagination__btn--last-page', $customPagination),
-                    $nextPageBtn = $('.custom-pagination__btn--next', $customPagination),
-                    $prevPageBtn = $('.custom-pagination__btn--prev', $customPagination),
-                    $gridToolbar = (e.element.find('.dx-toolbar-items-container').length > 0)
-                            ? e.element.find('.dx-datagrid-header-panel .dx-toolbar-items-container')
-                            : e.element.find('.dx-datagrid-header-panel'),
-                    $appendedPagination = $('.custom-pagination.custom-pagination--certificates', $gridToolbar);
-
-            if (pageCount > 1) {
-              $select.empty();
-              for (let i = 0; i < pageCount; i++) {
-                (i === currentPage)
-                        ? $select.append(`<option selected="selected" value="${i}">${i + 1}</option>>`)
-                        : $select.append(`<option value="${i}">${i + 1}</option>>`);
-              }
-
-              $appendedPagination.remove();
-              $gridToolbar.append($customPagination);
-
-              if (currentPage === 0) {
-                $firstPageBtn.attr('disabled', true);
-                $prevPageBtn.attr('disabled', true);
-              }
-              else {
-                $firstPageBtn.attr('disabled', false);
-                $prevPageBtn.attr('disabled', false);
-              }
-
-              if ((currentPage + 1) === pageCount) {
-                $lastPageBtn.attr('disabled', true);
-                $nextPageBtn.attr('disabled', true);
-              }
-              else {
-                $lastPageBtn.attr('disabled', false);
-                $nextPageBtn.attr('disabled', false);
-              }
-
-              $select.on('change', function() {changePage($(this).val(), pageCount)});
-              $firstPageBtn.on('click', function() {changePage(0, pageCount)});
-              $lastPageBtn.on('click', function() {changePage(pageCount - 1)});
-              $nextPageBtn.on('click', function() {changePage(currentPage + 1)});
-              $prevPageBtn.on('click', function() {changePage(currentPage - 1)});
-
-              $customPagination.fadeIn('fast');
-            }
-          },
-          columns: [
-            {
-              dataField: 'certificateId',
-              visible: false
-            },
-            {
-              dataField: 'certificateNumber',
-              caption: '№ Сертифіката',
-              allowEditing: true,
-              allowFiltering: true
-            },
-            {
-              dataField: 'formNumber',
-              caption: '№ Форми',
-              allowEditing: false,
-              allowFiltering: true
-            },
-            {
-              type: "buttons",
-              width: 110,
-              cssClass: 'center-vertical',
-              visible: false,
-              buttons: [{
-                hint: 'Редагувати',
-                icon: 'edit',
-                onClick: e => {
-                  let organisationId = e.row.data.id;
-
-                  window.vue.$router.push(`/mariner/app/training-organisations/edit/${organisationId}`)
-                }
-              }]
-            }
-          ]
-        }
       }
     },
 
