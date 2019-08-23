e56947f8f46f2efed6212d843dea788fe036c5a5
Andrew Borodyn <andbor1rey@gmail.com>
2019-08-23 16:36:08 +0300

    add allow_function and level column in cert grid

--- front/src/views/Certificates.vue
+++ front/src/views/Certificates.vue
@@ -569,6 +569,18 @@
               allowFiltering: true,
             },
             {
+                dataField: 'allow_functions',
+                caption: 'Рівень функцій',
+                allowEditing: false,
+                allowFiltering: true
+            },
+            {
+                dataField: 'level',
+                caption: 'Рівень кваліфікації',
+                allowEditing: false,
+                allowFiltering: true
+            },
+            {
               dataField: 'sailorId',
               visible: false,
             },
@@ -764,7 +776,9 @@
                 blankNumber: cert.form_number,
                 issueDate: cert.date_of_issue,
                 validDate: cert.valid_date,
-                trainingDirection: `${cert.direction_title_cert} (${cert.direction_allow_functions}/${cert.direction_level})`,
+                trainingDirection: cert.direction_title_cert,
+                allow_functions: cert.direction_allow_functions,
+                level: cert.direction_level,
                 sailorId: cert.sailor_id,
                 sailor: `${cert.last_name_ukr} ${cert.first_name_ukr} ${cert.second_name_ukr}`,
                 trainigOrganisation: cert.organisation_name_cert,
@@ -779,8 +793,6 @@
               this.firstNamesUkr.push(cert.first_name_ukr);
               this.secondNamesUkr.push(cert.second_name_ukr);
             });
-
-              console.log(this.firstNamesUkr);
 
             let certsGrid = this.$refs.certsGrid.tableInstance,
                 selected = (certsGrid._options.selection.mode === 'multiple') ? `, Вибрано: ${certsGrid.getSelectedRowKeys().length}` : '';
